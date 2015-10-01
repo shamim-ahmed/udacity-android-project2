836685d59227198deb9f3c264306900ddb1f9f5e
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-10-01 23:23:19 +1000

    The movie id is saved as part of Movie object. This can be used in constructing queries later.

--- app/src/main/java/edu/udacity/android/popularmovies/Movie.java
+++ app/src/main/java/edu/udacity/android/popularmovies/Movie.java
@@ -8,6 +8,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Movie implements Parcelable {
+    private final Long id;
     private final String title;
     private final String releaseDate;
     private final Uri posterUri;
@@ -28,6 +29,7 @@ public class Movie implements Parcelable {
     };
 
     public Movie(JSONObject movieData, Uri posterBaseUri) throws JSONException {
+        id = movieData.getLong("id");
         title = movieData.getString("title");
         releaseDate = movieData.getString("release_date");
         String posterPath = movieData.getString("poster_path");
@@ -41,11 +43,16 @@ public class Movie implements Parcelable {
 
     public Movie(Parcel in) {
         Object[] values = in.readArray(ClassLoader.getSystemClassLoader());
-        title = (String) values[0];
-        releaseDate = (String) values[1];
-        posterUri = (Uri) values[2];
-        voteAverage = (Double) values[3];
-        synopsis = (String) values[4];
+        id = (Long) values[0];
+        title = (String) values[1];
+        releaseDate = (String) values[2];
+        posterUri = (Uri) values[3];
+        voteAverage = (Double) values[4];
+        synopsis = (String) values[5];
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -80,6 +87,6 @@ public class Movie implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeArray(new Object[]{title, releaseDate, posterUri, voteAverage, synopsis});
+        dest.writeArray(new Object[]{id, title, releaseDate, posterUri, voteAverage, synopsis});
     }
 }
