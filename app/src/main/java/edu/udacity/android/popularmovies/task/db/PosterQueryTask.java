ede64fc04d734a03bd7f447da76e36d60099fec5
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-11-04 21:59:05 +1100

    removed an unused import

--- app/src/main/java/edu/udacity/android/popularmovies/task/db/PosterQueryTask.java
+++ app/src/main/java/edu/udacity/android/popularmovies/task/db/PosterQueryTask.java
@@ -8,7 +8,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.ImageView;
 
